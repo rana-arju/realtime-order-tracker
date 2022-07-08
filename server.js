6d636c137dc3497f83756b1e169834fe9d9ec38f
rana-arju <arjurana20@gmail.com>
2022-07-07 21:22:04 -0700

    food ordering app primary bootraping

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 5000;
+
+
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+//set Template engine
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "./resources/views"));
+app.set('view engine', 'ejs');
+
+app.listen(port, () => {
+    console.log("server is running on PORT: ", port);
+})
